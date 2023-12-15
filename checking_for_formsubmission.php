887a6f4525929d0787b165635e0baad99cc5b926
tony555t <tonytullah764@gmail.com>
2023-12-15 10:53:36 +0300

    validation

--- checking_for_formsubmission.php
+++ checking_for_formsubmission.php
@@ -1,5 +1,9 @@
 <?php
 if(isset($_post['submit'])){
+    $name =array("edwine","student","peter","antony","kimani","mark","tim");
+    $minimum = 5;
+    $maximum =10;
+
     
    $username = $_POST["username"];
     $password = $_POST["password"];
@@ -7,11 +11,21 @@ if(isset($_post['submit'])){
  //echo "your password".$password;
 
 
- if (strlen($username)<5){
+ if (strlen($username)< $minimum){
     echo "username has to be longer than 5";
  }
+if(strlen($username)>$maximum){
+    echo"username is too long";
 }
-
+if(in_array($username,$name)){
+    echo "user name already exists";
+}
+if(!in_array($username,$name)){
+    echo "sorry acess denied";
+}else{
+    "welcome";
+}
+}
 
 ?>
 <!DOCTYPE html>
